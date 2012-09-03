2c24833bbbc282650118dc393defb64ef16d8965
Peter Beardshear <pbeardshear@berkeley.edu>
2012-09-03 00:44:31 -0700

    fixed requires, and moved from node-static to send

--- examples/simple/server.js
+++ examples/simple/server.js
@@ -2,13 +2,13 @@ var tycho = require('../../lib/tycho');
 
 tycho.createServer().listen(3000);
 
-tycho.Routes.sample = function (internal, message) {
-	tycho.out.log('in routing');
-	this.send('Beep boop, received sample message', 'response');
-};
+// tycho.Routes.sample = function (internal, message) {
+	// tycho.out.log('in routing');
+	// this.send('Beep boop, received sample message', 'response');
+// };
 
-tycho.Routes.another = function (internal, message) {
-	// Echo back received message
-	console.log(message);
-	this.send('Echoing received message:\n' + message.data[1], 'response');
-};
+// tycho.Routes.another = function (internal, message) {
+	////Echo back received message
+	// console.log(message);
+	// this.send('Echoing received message:\n' + message.data[1], 'response');
+// };
